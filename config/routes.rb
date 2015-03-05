beb051fb37b52f527dcd80ac6a97b13bd1c2b073
Merlin Kötzing <on30f7h3m4ny@gmail.com>
2015-03-05 16:09:09 +0100

    fixed user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'users#index'
-
+  # root to: 'welcome#index'
+  
+  # only use these when logged in
+  devise_scope :user do
+    root to: 'welcome#index'
+  end
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150305114300) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "password_digest",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -27,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150305114300) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.string   "password_digest",        limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
