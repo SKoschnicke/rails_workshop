88794d9a56f9c802264e0418ec48557d3d357967
Flemming Stäbler <stu116021@mail.uni-kiel.de>
2015-03-06 09:38:12 +0100

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305142833) do
+ActiveRecord::Schema.define(version: 20150306080046) do
 
   create_table "teams", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20150305142833) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "password_digest",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
