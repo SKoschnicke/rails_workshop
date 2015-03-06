9782d35c3f47b4ab34490e4b06ae5faf376bcc99
Merlin Kötzing <on30f7h3m4ny@gmail.com>
2015-03-06 16:01:58 +0100

    Added User links, converted erb to haml and worked on navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, uniqueness: true
+  # this is all done by the "devise" module
+  # validates :email, uniqueness: true
   validates :name, allow_blank: false, presence: true
 
   has_and_belongs_to_many :teams
