b82b242579a80e1044615674a2f11e3d178eea5c
Valentin Buck <apexys@toppoint.de>
2015-03-06 09:42:38 +0100

    Regenerated schema and fixed contract naming

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,11 +3,10 @@ class Contract < ActiveRecord::Base
     has_many :timeentries
     validates :hours, numericality: {greater_than_or_equal_to: 0}
     validates :wage, numericality: {greater_than_or_equal_to: 0}
-    validate :validate_end_date_before_start_date
+    validate :validate_start_date_before_end_date
     
-    def validate_end_date_before_start_date
+    def validate_start_date_before_end_date
         if self.end_date && self.start_date
-            Rails.logger.debug self.inspect
             errors.add(:end_date, " needs to be after start date") if self.end_date < self.start_date
         end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305132943) do
+ActiveRecord::Schema.define(version: 20150306080046) do
 
   create_table "contracts", force: :cascade do |t|
     t.integer  "hours",      limit: 4
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20150305132943) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "password_digest",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
