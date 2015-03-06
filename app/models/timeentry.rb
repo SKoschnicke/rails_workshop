b6a4703356229e6536b0ce70971293772264606d
Malte-D <delfsmalte@gmail.com>
2015-03-06 09:49:58 +0100

    added validation for duration

--- app/models/timeentry.rb
+++ app/models/timeentry.rb
@@ -2,4 +2,6 @@ class Timeentry < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :user
   belongs_to :contract
+  
+  validates :duration, numericality: {greater_than_or_equal_to: 0, only_integer: true}
 end
